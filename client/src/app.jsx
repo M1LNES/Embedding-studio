a0198b3725bd1610787033bf79a2a3adcbc69d08
Milan Janoch <milan.janoch@emplifi.io>
2024-03-22 09:29:31 +0100

    Uz na to fakt nemam

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -2,24 +2,12 @@ import React from 'react'
 import Playground from './components/playground'
 import { Provider } from 'react-redux'
 import { store } from './state/store'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-
-const queryClient = new QueryClient()
 
 const App = () => {
 	return (
-		<BrowserRouter>
-			<QueryClientProvider client={queryClient}>
-				<Provider store={store}>
-					<Routes>
-						<Route path='/idk' element={<h2>ahoj</h2>} />
-
-						<Route path='/' element={<Playground />} />
-					</Routes>
-				</Provider>
-			</QueryClientProvider>
-		</BrowserRouter>
+		<Provider store={store}>
+			<Playground />
+		</Provider>
 	)
 }
 
